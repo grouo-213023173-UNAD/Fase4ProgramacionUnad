bd755ab3e78f0cf247780e72657abb95fe290ae1
DS <Diazsantiago@gmail.com>
2026-05-14 16:39:06 -0500

    feat:implementar clase cliente con validaciones e pequeños arreglos

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from models.cliente import Cliente
 from exceptions.custom_exceptions import ClienteInvalidoError
 
 
 try:
 
-    raise ClienteInvalidoError("El cliente no tiene un email válido")
+    cliente1 = Cliente(
+        "Daniel",
+        "123456",
+        "daniel@gmail.com"
+    )
+
+    print(cliente1.mostrar_info())
 
 except ClienteInvalidoError as error:
 
-    print(f"Error detectado: {error}")
+    print(f"Error: {error}")
---
+++ models/cliente.py
@@ -0,0 +1,20 @@
+from exceptions.custom_exceptions import ClienteInvalidoError
+
+class Cliente:
+    def __init__(self, nombre, documento, email):
+        # Validaciones para el cliente        
+        if not nombre.strip(): 
+            raise ClienteInvalidoError("El nombre del cliente no puede estar vacío") 
+        
+        if "@" not in email or "." not in email:
+            raise ClienteInvalidoError("El email del cliente no es válido")
+        
+        if not documento.strip():
+            raise ClienteInvalidoError("El documento del cliente no puede estar vacío")
+        
+        self.__nombre = nombre
+        self.__documento = documento
+        self.__email = email
+
+    def mostrar_info(self):
+        return f"Cliente: {self.__nombre}, Documento: {self.__documento}, Email: {self.__email}"
